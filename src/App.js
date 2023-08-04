9d6bed32551b65bc1cee870a151d2a4407745689
Jorge Levy <jorge.levy@jorgelevy.com>
2023-08-03 19:30:22 -0700

    data api

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  
-    useEffect(() => {
-      fetch("api/Weather")
-        .then(response => response.text())
-        .then(data => console.log(data));
-    }, []);
-  
-    return (
-      <div className="App">
+
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("api/products")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setProducts(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, []);
+
+  return (
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+        <ul>
+          {products.map(product => (
+            <li key={product.id}>{product.name}</li>
+          ))}
+        </ul>
+        {products.length} products found.
+      </div>
     </div>
   );
 }
